939cd5ee06000860714e546a4e5f88d408f43df7
SirSimon04 <simi@engelnetz.de>
2022-02-27 04:29:36 +0100

    remove cors

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify, send_file, Response
 import werkzeug.utils as w
-from flask_cors import CORS
 import time
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
